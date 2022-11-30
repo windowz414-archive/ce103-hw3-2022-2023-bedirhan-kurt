848c4fd1fdf369b87c223ef1b0ec8919ef22a91e
Bushra KURT <bushra.kurt@outlook.com>
2022-11-30 13:57:31 +0300

    LbrrnMenu: Implement options for listing and searching
    * Also code people section along.
    
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce103-hw3-library-app/LbrrnMenu.cs
+++ ce103-hw3-library-app/LbrrnMenu.cs
@@ -28,8 +28,7 @@ namespace ce103_hw3
             }
             else if (pointedIndex == 2)
             {
-                // TODO: Make a student management submenu and call it here.
-                throw new NotImplementedException();
+                LbrrnPeople();
 
             }
             else if (pointedIndex == 3)
@@ -70,12 +69,16 @@ namespace ce103_hw3
                 // TODO: Code a selection menu with categories and the options to delete, choose something else or return from there and assign its variables here.
                 throw new NotImplementedException();
             }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
         }
         private void LbrrnBooks()
         {
             string booksPromptIcon = "?";
             string booksPrompt = "What will you do about books?";
-            string[] booksOptions = { "Add a new book", "Edit an exinsting book", "Delete an existing book" };
+            string[] booksOptions = { "Add a new book", "Edit an exinsting book", "Delete an existing book", "List all books", "Search a specific book" };
             LbrrnMenuCnstrctr lbrrnBooks = new LbrrnMenuCnstrctr(booksPromptIcon, booksPrompt, booksOptions);
 
             int pointedIndex = lbrrnBooks.Choose();
@@ -93,6 +96,49 @@ namespace ce103_hw3
             {
                 // TODO: Code a selection menu with categories and the options to delete, choose something else or return from there and assign its variables here.
                 throw new NotImplementedException();
+            }
+            else if (pointedIndex == 3)
+            {
+                // TODO: Code a list menu that reads the file and lists whatever it reads, along with a back button.
+                throw new NotImplementedException();
+            }
+            else if (pointedIndex == 4)
+            {
+                // TODO: Code a search menu that asks search method - ID, author and name.
+                throw new NotImplementedException();
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        private void LbrrnPeople()
+        {
+            string peoplePromptIcon = "?";
+            string peoplePrompt = "What will you do about people?";
+            string[] peopleOptions = { "Add a person borrowing book and what book they borrowed", "Update a previous person's status", "List people borrowed books from this library" };
+            LbrrnMenuCnstrctr lbrrnPeople = new LbrrnMenuCnstrctr(peoplePromptIcon, peoplePrompt, peopleOptions);
+
+            int pointedIndex = lbrrnPeople.Choose();
+            if (pointedIndex == 0)
+            {
+                // TODO: Code an add menu with text edit functionalities and assign its variables here.
+                throw new NotImplementedException() ;
+            }
+            else if (pointedIndex == 1)
+            {
+                // TODO: Code a selection menu with updating options.
+                throw new NotImplementedException();
+            }
+            else if (pointedIndex == 2)
+            {
+                // TODO: Code a list menu that reads the file and lists whatever it reads, along with a back button.
+                throw new NotImplementedException();
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
             }
         }
     }
