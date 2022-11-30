a5014cf507b814f1c78f8219cbd4ae959e0b925e
Bushra KURT <bushra.kurt@outlook.com>
2022-11-30 18:10:52 +0300

    LbrrnMenu: Implement "Go back" button
    * Basically goes a menu up.
    
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce103-hw3-library-app/LbrrnMenu.cs
+++ ce103-hw3-library-app/LbrrnMenu.cs
@@ -54,7 +54,7 @@ namespace ce103_hw3
         {
             string ctgryPromptIcon = "?";
             string ctgryPrompt = "What will you do about book categories?";
-            string[] ctgryOptions = { "Add a new category", "Edit an existing category", "Delete an existing category" };
+            string[] ctgryOptions = { "Add a new category", "Edit an existing category", "Delete an existing category", "Go back" };
             LbrrnMenuCnstrctr lbrrnBookCtgry = new LbrrnMenuCnstrctr(ctgryPromptIcon, ctgryPrompt, ctgryOptions);
 
             int pointedIndex = lbrrnBookCtgry.Choose();
@@ -73,6 +73,10 @@ namespace ce103_hw3
                 // TODO: Code a selection menu with categories and the options to delete, choose something else or return from there and assign its variables here.
                 throw new NotImplementedException();
             }
+            else if (pointedIndex == 3)
+            {
+                LbrrnMenu();
+            }
             else
             {
                 throw new IndexOutOfRangeException();
@@ -82,7 +86,7 @@ namespace ce103_hw3
         {
             string booksPromptIcon = "?";
             string booksPrompt = "What will you do about books?";
-            string[] booksOptions = { "Add a new book", "Edit an exinsting book", "Delete an existing book", "List all books", "Search a specific book" };
+            string[] booksOptions = { "Add a new book", "Edit an exinsting book", "Delete an existing book", "List all books", "Search a specific book", "Go back" };
             LbrrnMenuCnstrctr lbrrnBooks = new LbrrnMenuCnstrctr(booksPromptIcon, booksPrompt, booksOptions);
 
             int pointedIndex = lbrrnBooks.Choose();
@@ -111,6 +115,10 @@ namespace ce103_hw3
                 // TODO: Code a search menu that asks search method - ID, author and name.
                 throw new NotImplementedException();
             }
+            else if (pointedIndex == 5)
+            {
+                LbrrnMenu();
+            }
             else
             {
                 throw new IndexOutOfRangeException();
@@ -121,7 +129,7 @@ namespace ce103_hw3
         {
             string peoplePromptIcon = "?";
             string peoplePrompt = "What will you do about people?";
-            string[] peopleOptions = { "Add a person borrowing book and what book they borrowed", "Update a previous person's status", "List people borrowed books from this library" };
+            string[] peopleOptions = { "Add a person borrowing book and what book they borrowed", "Update a previous person's status", "List people borrowed books from this library", "Go back" };
             LbrrnMenuCnstrctr lbrrnPeople = new LbrrnMenuCnstrctr(peoplePromptIcon, peoplePrompt, peopleOptions);
 
             int pointedIndex = lbrrnPeople.Choose();
@@ -139,6 +147,10 @@ namespace ce103_hw3
             {
                 // TODO: Code a list menu that reads the file and lists whatever it reads, along with a back button.
                 throw new NotImplementedException();
+            }
+            else if (pointedIndex == 3)
+            {
+                LbrrnMenu();
             }
             else
             {
