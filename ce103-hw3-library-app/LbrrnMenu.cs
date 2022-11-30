a868bd7d0927771c414cebc4e9b6e35d803ead82
Bushra KURT <bushra.kurt@outlook.com>
2022-11-30 13:16:01 +0300

    LbrrnMenu: Implement book operations menu
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce103-hw3-library-app/LbrrnMenu.cs
+++ ce103-hw3-library-app/LbrrnMenu.cs
@@ -15,17 +15,16 @@ namespace ce103_hw3
             string mainPromptIcon = "?";
             string mainPrompt = "What will you work with?";
             string[] mainOptions = { "Book Categories", "Books", "People", "Log out" };
-            LbrrnMenuCnstrctr lbrrnMenuCnstrctr = new LbrrnMenuCnstrctr(mainPromptIcon, mainPrompt, mainOptions);
+            LbrrnMenuCnstrctr lbrrnMenu = new LbrrnMenuCnstrctr(mainPromptIcon, mainPrompt, mainOptions);
 
-            int pointedIndex = lbrrnMenuCnstrctr.Choose();
+            int pointedIndex = lbrrnMenu.Choose();
             if (pointedIndex == 0)
             {
                 LbrrnBookCategories();
             }
             else if (pointedIndex == 1)
             {
-                // TODO: Make a book management submenu and call it here.
-                throw new NotImplementedException();
+                LbrrnBooks();
             }
             else if (pointedIndex == 2)
             {
@@ -49,9 +48,9 @@ namespace ce103_hw3
             string ctgryPromptIcon = "?";
             string ctgryPrompt = "What will you do about book categories?";
             string[] ctgryOptions = { "Add a new category", "Edit an existing category", "Delete an existing category" };
-            LbrrnMenuCnstrctr lbrrnMenu = new LbrrnMenuCnstrctr(ctgryPromptIcon, ctgryPrompt, ctgryOptions);
+            LbrrnMenuCnstrctr lbrrnBookCtgry = new LbrrnMenuCnstrctr(ctgryPromptIcon, ctgryPrompt, ctgryOptions);
 
-            int pointedIndex = lbrrnMenu.Choose();
+            int pointedIndex = lbrrnBookCtgry.Choose();
             if (pointedIndex == 0)
             {
                 // TODO: Code an add menu with text edit functionalities and assign its variables here.
@@ -67,7 +66,30 @@ namespace ce103_hw3
                 // TODO: Code a selection menu with categories and the options to delete, choose something else or return from there and assign its variables here.
                 throw new NotImplementedException();
             }
+        }
+        private void LbrrnBooks()
+        {
+            string booksPromptIcon = "?";
+            string booksPrompt = "What will you do about books?";
+            string[] booksOptions = { "Add a new book", "Edit an exinsting book", "Delete an existing book" };
+            LbrrnMenuCnstrctr lbrrnBooks = new LbrrnMenuCnstrctr(booksPromptIcon, booksPrompt, booksOptions);
 
+            int pointedIndex = lbrrnBooks.Choose();
+            if (pointedIndex == 0)
+            {
+                // TODO: Code an add menu with text edit functionalities and assign its variables here.
+                throw new NotImplementedException();
+            }
+            else if (pointedIndex == 1)
+            {
+                // TODO: Code an edit menu with selection and edit functionalities and assign its variables here.
+                throw new NotImplementedException();
+            }
+            else if (pointedIndex == 2)
+            {
+                // TODO: Code a selection menu with categories and the options to delete, choose something else or return from there and assign its variables here.
+                throw new NotImplementedException();
+            }
         }
     }
 }
