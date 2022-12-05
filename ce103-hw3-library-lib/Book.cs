a8fee9bb9b86eecd857d8aff66e626c9610a6f57
Bushra KURT <bushra.kurt@outlook.com>
2022-12-05 17:39:20 +0300

    [CHORE] lib/Book: Remove unnecesarry `using`s
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce103-hw3-library-lib/Book.cs
+++ ce103-hw3-library-lib/Book.cs
@@ -9,11 +9,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ce103_hw3_lib
 {
