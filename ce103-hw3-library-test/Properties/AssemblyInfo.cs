813099c77424873f14151f39167b822d982177e1
Bushra KURT <bushra.kurt@outlook.com>
2022-12-05 17:59:56 +0300

    CE103-HW3: Initial Setup and Test projects
    * FML!
    
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

---
+++ ce103-hw3-library-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ce103-hw3-library-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ce103-hw3-library-test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b04c73ce-ed4f-4fec-9da7-b9ec802ca852")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ce103-hw3-library-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ce103_hw3_library_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
